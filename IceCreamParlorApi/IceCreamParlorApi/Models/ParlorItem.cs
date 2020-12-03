1104fb4568f52ba541ecd581352a6e0caaaadf5d
ReetuPriyanka Madanu <balaanthony1219@gmail.com>
2020-12-02 21:48:27 -0500

    Added a POCO class for Ice cream parlor items

---
+++ IceCreamParlorApi/IceCreamParlorApi/Models/ParlorItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IceCreamParlorApi.Models
+{
+    public class ParlorItem
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Flavor { get; set; }
+        public DateTime IntroducedDate { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
